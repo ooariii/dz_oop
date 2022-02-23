3e963c9061ce2c8902bd394069c3e5af788efb2c
ooariii <rusakovicharina@mail.ru>
2022-02-23 18:48:06 +0300

    Add files via upload

---
+++ dz_oop2.py
@@ -0,0 +1,40 @@
+import string
+
+class Alphabet:
+
+    def __init__(self, language, letters_str):
+        self.lang = language
+        self.letters = list(letters_str)
+
+    def print(self):
+        print(self.letters)
+
+    def letters_num(self):
+        len(self.letters)
+
+class EngAlphabet(Alphabet):
+
+    __letters_num = 26
+
+    def __init__(self):
+        super().__init__('En', string.ascii_uppercase)
+
+    def letters_num(self):
+        return EngAlphabet.__letters_num
+
+    def is_en_letter(self, letter):
+        if letter.upper() in self.letters:
+            return True
+        return False
+
+    @staticmethod
+    def example():
+        print("English Example:\nSomething in english")
+
+if __name__ == '__main__':
+    eng_alphabet = EngAlphabet()
+    eng_alphabet.print()
+    print(eng_alphabet.letters_num())
+    print(eng_alphabet.is_en_letter('F'))
+    print(eng_alphabet.is_en_letter('Щ'))
+    EngAlphabet.example()
